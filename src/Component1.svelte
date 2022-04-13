b0efa4618e31f0914a06fa212071fbd5bb85a5be
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-14 00:36:30 +0200

    ok

--- src/Component1.svelte
+++ src/Component1.svelte
@@ -1,3 +1,21 @@
+<script>
+	import Modal from './Modal.svelte'
+	import App from './App.svelte'
+	import Component1 from './Component1.svelte'
+	import Component2 from './Component2.svelte'
+	import Component3 from './Component3.svelte'
+
+	// initialise modal state and content
+	let showModal = false;
+	let modalContent;
+	
+	// pass in component as parameter and toggle modal state
+	function toggleModal(component) {
+		modalContent = component;
+		showModal = !showModal;
+	}
+</script>
+
 <div>
 	<ol>
 		<li><button>
@@ -9,13 +27,11 @@
 		</button></li>
 		<li>Sign up and confirm the registration email (answer the questions with "i dont know" if you like)
 			and choose a servername and your own region(you strengthen the connection in your own region, other donators in their region)</li>
-		<li><button>
-			<a
-            href="https://github.com/draeder/donate-decentralize"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Drop Support URL</a>	 
+		<li><button on:click={() => (toggleModal(Component3))}>
+			Drop Support URL
+		</button>	 
 		</li>
 	
 	</ol>
 </div>
+
