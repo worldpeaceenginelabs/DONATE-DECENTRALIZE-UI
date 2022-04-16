451b7e2446e56842d0700a5faee81466f124d8a2
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-16 19:21:57 +0200

    setContext - getContext

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,31 @@
+<script>
+	// Enter your apps name
+	let appname = "Your App's name";
+	setContext("name", appname);
+	// Enter your button texts
+	let button1 = "Donate Decentralize";
+	let button2 = "Selfhost Decentralize";
+	let button3 = "Drop Support URL"
+
+	import Modal from './Modal.svelte'
+	import Component1 from './Component1.svelte'
+	import Component2 from './Component2.svelte'
+	import Component3 from './Component3.svelte'
+	import {setContext} from "svelte"
+
+	
+
+	// initialise modal state and content
+	let showModal = false;
+	let modalContent;
+	
+	// pass in component as parameter and toggle modal state
+	function toggleModal(component) {
+		modalContent = component;
+		showModal = !showModal;
+	}
+</script>
+
 <div align="right" style="background-color:LightGrey">
 <strong>Do you love {appname}? Donate a P2P tracker to make sure {appname} keeps running! For free or for 7$ monthly! Your donation goes 100% were it belongs to... </strong>
 <button on:click={() => (toggleModal(Component1))}>
@@ -12,29 +40,5 @@
 </div>
 
 {#if showModal}
-	<Modal on:click={toggleModal} {modalContent} />
+	<Modal on:click={toggleModal} {modalContent}/>
 {/if}
-
-<script>
-	// Enter your apps name
-	let appname = "(Your App's Name)";
-	// Enter your button texts
-	let button1 = "Donate Decentralize";
-	let button2 = "Selfhost Decentralize";
-	let button3 = "Drop Support URL"
-
-	import Modal from './Modal.svelte'
-	import Component1 from './Component1.svelte'
-	import Component2 from './Component2.svelte'
-	import Component3 from './Component3.svelte'
-
-	// initialise modal state and content
-	let showModal = false;
-	let modalContent;
-	
-	// pass in component as parameter and toggle modal state
-	function toggleModal(component) {
-		modalContent = component;
-		showModal = !showModal;
-	}
-</script>
--- src/Component3.svelte
+++ src/Component3.svelte
@@ -1,12 +1,21 @@
+<script>
+	// Enter your apps name
+	//let appname = "(Your App's Name)";
+	// export var appname;
+	import {getContext} from "svelte"
+	let appname1 = getContext("name");
+
+
+	
+</script>
+
 <div align="center">
 	<input>
 	<button>Drop tracker URL</button>
 	<p>Thank you so much for supporting us.<br><br>
-	With your tracker donation, you are supporting the whole community of {appname} users actively.<br>
+	With your tracker donation, you are supporting the whole community of {JSON.stringify(appname1)} users actively.<br>
 </p>
 </div>
 
-<script>
-	// Enter your apps name
-	let appname = "(Your App's Name)";
-</script>
+
+
