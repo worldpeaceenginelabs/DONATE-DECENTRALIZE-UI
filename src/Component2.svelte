63990c3ddab03bfb644f417ee9900d4c49b4cfff
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-05-30 23:54:40 +0200

    linked repos, commented for devs

--- src/Component2.svelte
+++ src/Component2.svelte
@@ -1,9 +1,8 @@
 <div>
 	<ol>
-		<li><button>Selfhost in your P2P Browser</button></li><br>
-		<li><button>Selfhost on your own server</button></li><br>
-		<li><button>Download Windows Client</button>	 
+		<li><button>Selfhost in your P2P Browser</button></li><br> <!-- Mockup, coming... -->
+		<li><button><a href="https://github.com/worldpeaceenginelabs/gun-simple-server" target="_blank">Selfhost on your own server</a></button></li><br> <!-- how to run it on node.js locally-->
+		<li><button><a href="https://github.com/worldpeaceenginelabs/gun-relay-desktop-win" target="_blank">Download Windows Client</a></button> <!--gun-relay-dektop-win readme.txt, insert already existing peers, compile exe, drop the downloadable file address in href-->
 		</li>
-	
 	</ol>
 </div>
